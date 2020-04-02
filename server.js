69a8f459418e48f4d377ff5c390e9c8a9a4b4b7b
yokevi <you.k2208@gmail.com>
2020-04-02 15:23:00 -0700

    added node and node packages, set up server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { join } = require('path')
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('mongoose').connection.once('open', () => app.listen(PORT))
